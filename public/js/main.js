3109b3570d5a4b02ebdbd2881e47bb427e164375
olexnick <olexnick@gmail.com>
2017-08-30 20:11:31 +0300

    may be the end

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,7 @@ $(document).ready(function(){
   let reduserBotton = '.home-botton';
 
   $('.li-home').bind('click', function(){
-    if(reduserHead == '.home'){
-      console.log( 'равно');
-
-    }else{
+    if(reduserHead != '.home'){
       $(reduserHead).slideUp(3000);
       setTimeout (function(){
         $('.home').slideDown(3000);
@@ -22,31 +19,10 @@ $(document).ready(function(){
         reduserHead = '.home';
         reduserBotton='.home-botton';
       }, 3800);
-      console.log( 'не равно');
     }
   });
 
     $('.li-product').bind('click', function(){
-      // if(reduserHead == '.product' ){
-      //   console.log("dhdhdhd");
-      // } else {
-      //   $(reduserHead).slideUp(3000);
-      //   setTimeout (function(){
-      //     $('.product').slideDown(3000);
-      //   }, 3010);
-      //   setTimeout (function(){
-      //     $(reduserBotton).fadeOut(1000);
-      //   }, 3500);
-      //   setTimeout (function(){
-      //     $('.product-bottom').fadeIn('slow','linear');
-      //   }, 3550);
-      //   setTimeout (function(){
-      //     reduserHead = '.product';
-      //     reduserBotton='.product-bottom';
-      //   }, 3800);
-      // }
-
-
       if(reduserHead != '.product'){
         $(reduserHead).slideUp(3000);
         setTimeout (function(){
@@ -81,7 +57,6 @@ $(document).ready(function(){
       }
     });
 
-
   $('.li-single').bind('click', function(){
     if(reduserHead !='.single-product'){
 
@@ -91,29 +66,78 @@ $(document).ready(function(){
       setTimeout (function(){
         $('.single-product').fadeIn('slow','linear');
       }, 1550);
-
-
-      // $(reduserHead).slideUp(3000);
-      // setTimeout (function(){
-      //   $('.single').slideDown(3000);
-      // }, 3010);
       setTimeout (function(){
         $(reduserBotton).fadeOut(1000);
       }, 3500);
-
       setTimeout (function(){
         reduserHead = '.single-product';
         reduserBotton='.single-product';
       }, 3800);
     }
-});
+  });
 
-
-
-
-
-
-
+  $('.homePage').bind('click', function(){
+    if(reduserHead != '.home'){
+      setTimeout (function(){
+        $(reduserHead).hide();
+        $(reduserBotton).hide();
+      }, 1500);
+      setTimeout (function(){
+        $('.home').show();
+        $('.home-botton').show();
+      }, 1500);
+      setTimeout (function(){
+        reduserHead = '.home';
+        reduserBotton='.home-botton';
+      }, 3800);
+    }
+  });
+  $('.productPage').bind('click', function(){
+    if(reduserHead != '.product'){
+      setTimeout (function(){
+        $(reduserHead).hide();
+        $(reduserBotton).hide();
+      }, 1500);
+      setTimeout (function(){
+        $('.product').show();
+        $('.product-bottom').show();
+      }, 1500);
+      setTimeout (function(){
+        reduserHead = '.product';
+        reduserBotton='.product-bottom';
+      }, 3800);
+    }
+  });
+  $('.blogPage').bind('click', function(){
+    if(reduserHead != '.blog'){
+      setTimeout (function(){
+        $(reduserHead).hide();
+        $(reduserBotton).hide();
+      }, 1500);
+      setTimeout (function(){
+        $('.blog').show();
+      }, 1500);
+      setTimeout (function(){
+        reduserHead = '.blog';
+        reduserBotton='.blog';
+      }, 3800);
+    }
+  });
+  $('.singlePage').bind('click', function(){
+    if(reduserHead != '.single-product'){
+      setTimeout (function(){
+        $(reduserHead).hide();
+        $(reduserBotton).hide();
+      }, 1500);
+      setTimeout (function(){
+        $('.single-product').show();
+      }, 1500);
+      setTimeout (function(){
+        reduserHead = '.single-product';
+        reduserBotton='.single-product';
+      }, 3800);
+    }
+  });
 
 // setInterval (function(){
 //     console.log(reduserHead,'reduserHead');
